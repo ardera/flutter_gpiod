fcc1016a06a7e572e79fd0efe19965d6a8d4807e
Hannes Winkler <hanneswinkler2000@web.de>
2020-12-23 04:28:53 +0100

    dont include build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -i
-
-pushd ./bindings_generator
-./generate_bindings.sh
-popd
-
-cmd.exe /C flutter build bundle --target=example/lib/main.dart
-
-rsync -a ./build/flutter_assets/ hpi4:/home/pi/devel/flutter_gpiod_assets
